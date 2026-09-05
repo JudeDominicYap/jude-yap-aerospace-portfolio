5eb6b69eb535a621ffb29c7c94bffe9bb23e65e5
JudeDominicYap <judedominic.yap@gmail.com>
2026-09-05 18:20:50 +0800

    Switch portfolio to aerospace flight-deck design

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
-import MossGrotto from "./pages/MossGrotto";
+import AerospacePortfolio from "./pages/AerospacePortfolio";
 
 export default function App() {
   return (
@@ -10,7 +10,7 @@ export default function App() {
       <ThemeProvider defaultTheme="dark">
         <TooltipProvider>
           <Toaster />
-          <MossGrotto />
+          <AerospacePortfolio />
         </TooltipProvider>
       </ThemeProvider>
     </ErrorBoundary>
