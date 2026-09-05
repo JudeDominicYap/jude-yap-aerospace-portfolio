fe05a468b9f59905988f9f9146d40b74193f9675
JudeDominicYap <judedominic.yap@gmail.com>
2026-09-05 09:02:57 +0800

    Switch application shell to Moss Grove portfolio

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,18 @@
-import FlightDeck from "./pages/FlightDeck";
+import { Toaster } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import ErrorBoundary from "./components/ErrorBoundary";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import MossGrotto from "./pages/MossGrotto";
 
 export default function App() {
-  return <FlightDeck />;
+  return (
+    <ErrorBoundary>
+      <ThemeProvider defaultTheme="dark">
+        <TooltipProvider>
+          <Toaster />
+          <MossGrotto />
+        </TooltipProvider>
+      </ThemeProvider>
+    </ErrorBoundary>
+  );
 }
